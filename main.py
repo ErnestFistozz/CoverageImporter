583a6b229bfdecc316223679d50e7596b172e7dc
Fistozz <mamba.fistozz@gmail.com>
2023-08-02 00:27:08 +0200

    uncommented codecov data method

--- main.py
+++ main.py
@@ -9,10 +9,10 @@ if __name__ == '__main__':
     codecov_repositories = helpers.read_from_csv('codecov_repositories.csv')
     coveralls_repositories = helpers.read_from_csv('coveralls_repositories.csv')
 
-    #for codecov_repo in codecov_repositories:
-    #    codecov = CodeCovCoverage(codecov_repo['org'],codecov_repo['repo'], codecov_repo['main_branch'])
-    #    data = coverage_importer.codecov_data(codecov, helpers, codecov_repo['language'])
-    #    helpers.save_into_file('codecovdata.csv', data)
+    for codecov_repo in codecov_repositories:
+       codecov = CodeCovCoverage(codecov_repo['org'],codecov_repo['repo'], codecov_repo['main_branch'])
+       data = coverage_importer.codecov_data(codecov, helpers, codecov_repo['language'])
+       helpers.save_into_file('codecovdata.csv', data)
 
     for coveralls_repo in coveralls_repositories:
         coveralls = CoverallsCoverage(coveralls_repo['org'],coveralls_repo['repo'], coveralls_repo['main_branch'])
