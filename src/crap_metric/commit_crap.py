c27f392601f5d50880a57df7ea48d989758af842
Ernest Mamba <ebmamba@shoprite.co.za>
2023-08-24 00:18:09 +0200

    adding support for crap metric - yet experimental

---
+++ src/crap_metric/commit_crap.py
@@ -0,0 +1,15 @@
+from pydriller import Commit
+
+class CrapMetric:
+    
+    def commit_crap_metric(commitInst: Commit, patch_coverage: float) -> float:
+        total_method_complexity = 0 
+        for m in commitInst.modified_files:
+            for changed_method in m.changed_methods:
+                method_details = changed_method.__dict__
+                total_method_complexity += method_details['complexity']
+        commit_crap = pow(total_method_complexity, 2)*pow(1 - (patch_coverage/100), 3) + total_method_complexity
+        return round(commit_crap, 3)
+
+if __name__ == '__main__':
+    print(__name__)
