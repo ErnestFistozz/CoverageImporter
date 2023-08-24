28ce012320f302e7b1fee82a3830eb597957981b
Ernest Mamba <ebmamba@shoprite.co.za>
2023-08-24 14:42:08 +0200

    cleaning coveralls method and handling errors properly

--- src/coverageimporter.py
+++ src/coverageimporter.py
@@ -10,45 +10,47 @@ class CoverageImporter:
     def coveralls_data(self, coveralls: CoverallsCoverage, helpers: Helpers):
         git_url = f'https://github.com/{coveralls.org()}/{coveralls.repo()}.git'
         builds = coveralls.collect_builds_data()
-        
         for build in builds:
             try:
                 for commit in Repository(git_url, single = build['commit_sha']).traverse_commits():
                     coveralls_commit_files = coveralls.fetch_source_files(commit.hash)
-                    executable_lines = executed_lines = 0 
+                    executable_lines = executed_lines = commit_crappiness =  0 
                     patch_extracts = PatchExtracts()
                     crap_metric = CrapMetric()
-
-                    if not isinstance(coveralls_commit_files, type(None)) and len(coveralls_commit_files) != 0:
+                    if coveralls_commit_files:
                         for m in commit.modified_files:
                             if helpers.index_finder(m.filename, coveralls_commit_files) != -1:
                                 coveral_file_path = helpers.index_finder(m.filename, coveralls_commit_files)
                                 coverage_array = coveralls.source_coverage_array( commit.hash,
                                                 coveralls_commit_files[coveral_file_path])
-                                if not isinstance(coverage_array, type(None)) and len(coverage_array) != 0:
+                                if coverage_array: # if the coverage array is not empty
                                     modified_lines  = [ line_number[0] for line_number in m.diff_parsed['added'] ]
                                     executable_lines += len([1 for line_number in modified_lines  if isinstance(coverage_array[line_number - 1], int)])
                                     executed_lines += len([1 for line_number in modified_lines  if isinstance(coverage_array[line_number - 1], int) and coverage_array[line_number - 1] > 0])
-                                else:
-                                    continue
-                        patch_files = patch_extracts.patch_files(commit, coveralls_commit_files)
-                        patch_size = patch_extracts.patch_sizes(commit, coveralls_commit_files)
-                        commit_crappiness = crap_metric.commit_crap_metric(commit, (executed_lines/executable_lines)*100)
-                    else:
-                        continue
+                    # patch related files
+                    patch_files = patch_extracts.patch_files(commit, coveralls_commit_files)
+                    patch_size = patch_extracts.patch_sizes(commit, coveralls_commit_files)
+                    build.update(patch_files)
+                    build.update(patch_size)                    
+                    # delta maintainability index
                     dmm_commit_size = commit.dmm_unit_size
                     dmm_commit_complexity = commit.dmm_unit_complexity
                     dmm_commit_interface = commit.dmm_unit_interface
                     delta_maintainibility_model = round((dmm_commit_size + dmm_commit_complexity + dmm_commit_interface)/3 , 3)
-                try:
-                    build['patch_coverage'] = round((executed_lines/executable_lines)*100, 3)
+                    # CRAP Metric and Patch Coverage
+                    if executable_lines == 0:
+                        build['patch_coverage'] = 0.0
+                        commit_crappiness = crap_metric.commit_crap_metric(commit,0)
+                    else:
+                        commit_crappiness = crap_metric.commit_crap_metric(commit, (executed_lines/executable_lines)*100)
+                        build['patch_coverage'] = round((executed_lines/executable_lines)*100, 3)
+                    # update dictionary to have delta maintainability and crap metric
                     build['repository_name'] = f'{coveralls.org()}/{coveralls.repo()}'
-                except ZeroDivisionError:
-                    build['patch_coverage'] = 0.0
-                    build['repository_name'] = f'{coveralls.org()}/{coveralls.repo()}'
-                finally:
-                    build.update(patch_files)
-                    build.update(patch_size)
+                    build['dmm_unit_size'] =  dmm_commit_size
+                    build['dmm_unit_complexity'] =  dmm_commit_complexity
+                    build['dmm_unit_interface'] =  dmm_commit_interface
+                    build['dmm'] =  delta_maintainibility_model
+                    build['crap_metric'] =  commit_crappiness
             except Exception:
                 continue
         return builds
