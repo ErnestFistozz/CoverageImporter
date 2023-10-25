78aaeb3e4dbdc0ce22d84870693b9f8aa3529c30
Ernest Mamba <ebmamba@shoprite.co.za>
2023-10-25 12:25:25 +0200

    added new repositories

--- main.codecov.py
+++ main.codecov.py
@@ -6,6 +6,7 @@ if __name__ == '__main__':
     helpers = Helpers()
     coverage_importer = CoverageImporter()
     codecov_repositories = helpers.repositories('codecov_repos.txt')
+
     for codecov_repo in codecov_repositories:
         codecov = CodeCovCoverage(codecov_repo[0], codecov_repo[1])
         data = coverage_importer.codecov_data(codecov, helpers)
--- main.coveralls.py
+++ main.coveralls.py
@@ -5,12 +5,9 @@ from src.helpers import Helpers
 if __name__ == '__main__':
     helpers = Helpers()
     coverage_importer = CoverageImporter()
-    # coveralls_repositories = helpers.read_from_csv('large_scale_paper_coveralls.csv')
-    # coveralls_repositories = helpers.read_from_csv('large_scale_paper_coveralls.csv')
     coveralls_repositories = helpers.repositories('coveralls_repos.txt')
+
     for coveralls_repo in coveralls_repositories:
-        print(f'ORGANISATION:{coveralls_repo[0]}')
-        print(f'REPOSITORY: {coveralls_repo[1]}')
         coveralls = CoverallsCoverage(coveralls_repo[0], coveralls_repo[1])
         data = coverage_importer.coveralls_data(coveralls, helpers)
         helpers.save_into_file('coverall_coverage_results.csv', data)
